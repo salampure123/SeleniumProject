ab36a0872c928e55a5f988f71f2fe11d56e9a910
LENOVO <LENOVO@LAPTOP-4328LLVK>
2023-04-03 23:55:53 +0530

    First commit

---
+++ hybridActiTime/src/genericPackage/IAutoConstant.java
@@ -0,0 +1,13 @@
+package genericPackage;
+
+public interface IAutoConstant {
+
+	String EXCEL_PATH="./data/ActiTimeExcelSheet.xlsx";
+	String PROP_PATH="./data/config.properties";
+	
+	String CHROME_KEY="webdriver.chrome.driver";
+	String GECKO_KEY="webdriver.gecko.driver";
+	
+	String CHROME_PATH="./drivers/chromedriver.exe";
+	String GECKO_PATH="./drivers/geckodriver.exe";
+}
---
+++ hybridActiTime/src/genericPackage/WorkLib.java
@@ -0,0 +1,10 @@
+package genericPackage;
+
+public class WorkLib extends BaseTest{
+	
+	public void handleConfirmationPopup()
+	{
+		
+		driver.switchTo().alert().accept();
+	}
+}
